0cd2da541ffaf9d2581cc1343c8e5125532ce024
Nana Baidoo <baidoolartey@gmail.com>
2020-04-21 06:33:31 -1200

    initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+#INSTALL PACKAGES
+*express 
+*ejs
+*mongodb
+
+#FRONT END
+USE EJS 
+home page 
+employee list page 
+to-do list
+
+#SERVER
+EXPRESS to create my server 
+connect to our database using mongodb client
+create our server app.js
+set the middleware the application should use 
+set up the port that the application will be listing 
+create a static array of employees
+
+#DATABASE
+create a database called employee database 
+create a to-do list collection
+upload the todo collection with data
---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const employeelist = require('./employeelist.json')
+const { MongoClient } = require('mongodb')
+const app = express();
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url, {useUnifiedTopology: true });
+let companyDb;
+let companyTodo;
+
+const liveConnect = async () => {
+    await client.connect();
+    companyDb = await client.db('employeedb');
+    companyTodo = await companyDb.collection('todo');
+}
+liveConnect();
+
+
+const createTodo = async (data) => {
+    try {
+        const result = await companyTodo.insertOne(data);
+        return result;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const getAll = async () => {
+    try {
+        const result = await companyTodo.find({}).toArray();
+        return result;
+    } catch (error) {
+        console.log(error)
+    }
+} 
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+    res.render('home', { employeelist });
+})
+
+app.get('/todoList', async (req, res)=>{
+    const todolist = await getAll();
+    res.render('todo', {
+        todolist
+    })
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log('App is listening on port 30000....')
+})
